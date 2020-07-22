4e5e856452986e245c1aaa01328eab901d8e3d3c
SiEr0212 <simonertel@gmx.de>
2020-07-22 15:42:50 +0200

    auth changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,59 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+import { Route, Redirect } from 'react-router-dom';
+//import Projects from './components/Projects';
+//import Navbar from './components/Navbar';
+//import ProjectDetails from './components/ProjectDetails';
+//import TaskDetails from './components/TaskDetails';
+import Signup from './components/Signup';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    user: this.props.user
+  }
+
+  setUser = user => {
+    this.setState({
+      user: user
+    })
+  }
+
+  render() {
+    return (
+      <div className='App' >
+        <Navbar user={this.state.user} setUser={this.setUser} />
+
+        <Route
+          exact path='/projects'
+          render={props => {
+            if (this.state.user) return <Projects {...props}/>
+            else return <Redirect to='/' />
+          }}
+        />
+        <Route
+          exact path='/projects/:id'
+          render={props => <ProjectDetails {...props} user={this.state.user} />}
+        />
+        <Route
+          exact path='/tasks/:id'
+          component={TaskDetails}
+        />
+        <Route
+          exact
+          path='/signup'
+          // to the Signup we have to pass a reference to the setUser method
+          // this we cannot do via component={<some component>}
+          // For this we use the render prop - The term “render prop” refers to a technique for sharing 
+          // code between React components using a prop whose value is a function.
+          // A component with a render prop takes a function that returns a React element and calls it 
+          // instead of implementing its own render logic.
+          render={props => <Signup setUser={this.setUser} {...props} />}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
