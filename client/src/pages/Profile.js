3960bf2bb8c90a5d18fa9785139a308a017dadcc
luisschmitz <luis.schmitz@web.de>
2020-07-22 15:36:23 +0200

    setup Profile page

---
+++ client/src/pages/Profile.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import EditPlace from '../components/EditPlace'
+
+
+const Profile = (props) => {
+  //console.log(props.data.email);
+  return (
+      <div>
+          <h1>My Profile</h1>
+          <EditPlace/>
+          
+    
+      </div>
+  )
+}
+
+export default Profile;
