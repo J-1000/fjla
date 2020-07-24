1aee501bd6344db55aa3e5a9dfd02cbbc750b5c4
SiEr0212 <simonertel@gmx.de>
2020-07-24 16:49:39 +0200

    updating to master2

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import Search from "./components/Search";
-<<<<<<< HEAD
 // import { Route, Redirect } from 'react-router-dom';
 import Profile from "./pages/Profile";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
@@ -11,23 +10,15 @@ import EditPlace from "./components/EditPlace";
 import MapBox from "./components/MapBox";
 import "./components/MapBox.css";
 import Home from "./components/Home";
-
 import Signup from "./components/SignUp";
 import Login from "./components/Login";
 // import Projects from './components/Projects';
 //import Navbar from './components/Navbar';
 //import ProjectDetails from './components/ProjectDetails';
 //import TaskDetails from './components/TaskDetails';
-=======
 import HoverRating from './components/Rating';
 // import { Route, Redirect } from 'react-router-dom';
-import Profile from "./pages/Profile";
-import { Route } from "react-router-dom";
-import EditPlace from "./components/EditPlace";
-import Signup from "./components/SignUp";
-import Login from "./components/Login";
 import Slider from "./components/Slider";
->>>>>>> 4f355964d867d6058b99249a8d623f9af3bd488d
 
 class App extends React.Component {
   state = {
@@ -42,7 +33,6 @@ class App extends React.Component {
 
   render() {
     return (
-<<<<<<< HEAD
       <div className="homeApp">
         <nav>
           <ToolNavbar user={this.state.user} setUser={this.setUser} />
@@ -50,24 +40,6 @@ class App extends React.Component {
 
         <div> Hello {this.state.user && this.state.user.username} </div>
         <Route
-=======
-      <div className="homeApp" style={{height:"100vh"}}>
-        <nav>
-          <ToolNavbar className="Nav ToolNavbar" user={this.state.user} setUser={this.setUser} />
-        </nav>
-     
-          <Route
-            exact
-            path="/myprofile"
-            render={props => <Profile {...props} setUser={this.setUser} />}
-          />
-          <Route
-            exact
-            path='/signup'
-            render={props => <Signup setUser={this.setUser} {...props} />}
-          />
-          <Route
->>>>>>> 4f355964d867d6058b99249a8d623f9af3bd488d
           exact
           path="/myprofile"
           render={(props) => <Profile {...props} setUser={this.setUser} />}
@@ -88,17 +60,6 @@ class App extends React.Component {
           path="/"
           render={(props) => (
             <div className="searchBar">
-<<<<<<< HEAD
-              {" "}
-              <Search />{" "}
-            </div>
-          )}
-        />
-       
-        <Route exact path="/home" render={(props) => <Home />} />
-      
-        </div>
-=======
             <div className="searchBar-inner">
             <Search />
               <Slider className="sliderComponent slide" />
@@ -109,8 +70,9 @@ class App extends React.Component {
             </div>
           )}
         />
+        <Route exact path="/home" render={(props) => <Home />} />
+      
       </div>
->>>>>>> 4f355964d867d6058b99249a8d623f9af3bd488d
     );
   }
 }
