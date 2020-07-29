2b9f8e139c00b66b57ff8d2246981e4a9a5b52be
luisschmitz <luis.schmitz@web.de>
2020-07-29 12:23:19 +0200

    added MongoDB URI

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const path         = require("path");
 const app = express();
 
 mongoose
-  .connect("mongodb://localhost/travelblog", {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI ||"mongodb://localhost/travelblog", {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
