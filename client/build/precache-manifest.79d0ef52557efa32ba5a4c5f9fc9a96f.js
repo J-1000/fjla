999da0edbb02298045b74cc9f651247765182b0d
SiEr0212 <simonertel@gmx.de>
2020-07-29 17:11:12 +0200

    woeked on navbar and logo styling

---
+++ client/build/precache-manifest.79d0ef52557efa32ba5a4c5f9fc9a96f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3a1605b5b4e97bcaed8108ac6e00a2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca0057866151ecbd8fd2",
+    "url": "/static/css/2.40b7ee1d.chunk.css"
+  },
+  {
+    "revision": "4305d1b7258c654157cf",
+    "url": "/static/css/main.d8d6168b.chunk.css"
+  },
+  {
+    "revision": "ca0057866151ecbd8fd2",
+    "url": "/static/js/2.dde1a11f.chunk.js"
+  },
+  {
+    "revision": "5599e968862052a1e150ba50f6354fc6",
+    "url": "/static/js/2.dde1a11f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4305d1b7258c654157cf",
+    "url": "/static/js/main.38e32c80.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "8b4f2d2ab6c733a2db82700365eb6cba",
+    "url": "/static/media/IMG_9232.8b4f2d2a.JPG"
+  },
+  {
+    "revision": "ee2be3697a4761c3ab4b5fd7cab01fb3",
+    "url": "/static/media/LOGOMYTENT.ee2be369.png"
+  }
+]);
